cfa68d07f5700aee68778b34de0dc72967279046
Nurmansyah <nurmanchoy@gmail.com>
2022-06-12 11:37:30 +0700

    feat: hitung hasil film sesuai emosi

--- promise.js
+++ promise.js
@@ -1,7 +1,23 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    //Gabung 2 array menjadi 1
+    const arrFilmIXX = await promiseTheaterIXX();
+    const arrFilmVGC = await promiseTheaterVGC();
+    const arrFilm = arrFilmIXX.concat(arrFilmVGC);
+
+    //Mengelompokkan hasil film sesuai emosi
+    const arrFilmEmosi = arrFilm.filter((element) => element.hasil === emosi);
+    console.log(arrFilmEmosi);
+
+    //Hitung jumlah film sesuai emosi
+    return arrFilmEmosi.length;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
